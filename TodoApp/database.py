1b8dc66f82b0c8c4dd7ce76ae1d2cc0cd19dfe42
codewith-bibek <bibek.sutradhar94@gmail.com>
2026-09-06 00:49:10 +0530

    prod database change

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,9 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+#
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+SQLALCHEMY_DATABASE_URL = 'postgresql://fastapi_owsd_user:7nQULPwBZmq1EsmJMzsV9kSv4bq5k5uW@dpg-dae6jq740ujc73e1ct1g-a.singapore-postgres.render.com/fastapi_owsd'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
